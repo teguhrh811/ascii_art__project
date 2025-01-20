cb76f935f13ce14df19f771a546fc5b7102cd71a
teguhrh811 <teguhprogarambang@gmail.com>
2025-01-20 12:05:46 +0800

    Add step 3

--- ascii_art.py
+++ ascii_art.py
@@ -2,6 +2,32 @@
 from PIL import Image
 import numpy as np
 
+def ascii_chars_separator(string):
+    arr_of_chars = []
+    for i in string:
+        arr_of_chars.append(i)
+
+    return arr_of_chars
+
+def ranged_int_to_char(ascii_chars, comparator, integers):
+    converted_integers = {}
+
+    for i in range(len(comparator)):
+    # for i in range(len(comparator) - 1):
+        if i == len(comparator) - 1: # the last char will be iterated
+            for j in range(comparator[i-1], comparator[i]): # we have to use range from the second latest num to the latest num + 1 
+            # because stop won't be stepped
+                if integers[j] >= comparator[i-1] and integers[j] <= comparator[i]:
+                    # converted_integers.append(ascii_chars[i])
+                    converted_integers[j] = ascii_chars[i]
+        else:
+            for j in range(comparator[i], comparator[i+1]): # the last char won't be iterated
+                if integers[j] >= comparator[i] and integers[j] < comparator[i+1]:
+                    # converted_integers.append(ascii_chars[i])
+                    converted_integers[j] = ascii_chars[i]
+    
+    return converted_integers
+
 # Read the image 
 img = Image.open("ww.jpeg") # Initialize variable named "img" with Image class from PIL
 
@@ -32,10 +58,14 @@ for y in range(len(img_pixel_brightness)): # represents the y point iterator
         img_pixel_brightness[y][x] = (sum(img.getpixel((x,y)))) // 3
 
 # 4. Convert brightness numbers to ASCII characters
-
+# BEGIN
 ascii_chars = "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"
 print(len(ascii_chars))
+comparator = np.linspace(0, 255, len(ascii_chars), dtype="int32")
+integers = np.linspace(0, 255, 256, dtype="int32")
 
+print(ranged_int_to_char(ascii_chars, comparator, integers))
 
+# 5. Print the ASCII art
 
 
