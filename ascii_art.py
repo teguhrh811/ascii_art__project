c99fb15f31a110508b8bd84b960edb5df58aa6f6
teguhrh811 <teguhprogarambang@gmail.com>
2025-01-21 10:13:16 +0800

    Updating comments

--- ascii_art.py
+++ ascii_art.py
@@ -1,13 +1,30 @@
-# Preparing libraries for image processing
+# 0. Preparing libraries for image processing
 from PIL import Image
 import numpy as np
 
-# def ascii_chars_separator(string):
-#     arr_of_chars = []
-#     for i in string:
-#         arr_of_chars.append(i)
+# User-defined functions
+def get_pixel(img):
+    img_height = img.height
+    img_width = img.width
+    img_pixel_rgb = np.zeros((3, img_height, img_width))
 
-#     return arr_of_chars
+    for channel in range(len(img_pixel_rgb)): # represents the channel iterator
+        for y in range(len(img_pixel_rgb[0])): # represents the y point iterator
+            for x in range(len(img_pixel_rgb[0][0])): # represents the x point iterator
+                img_pixel_rgb[channel][y][x] = img.getpixel((x,y))[channel]
+
+    return img_pixel_rgb
+
+def img_to_brightness_pixel(img):
+    img_height = img.height
+    img_width = img.width
+    img_pixel_brightness = np.zeros((img_height, img_width), dtype="int32") # initialize a variable that stores brightness pixels
+
+    for y in range(len(img_pixel_brightness)): # represents the y point iterator
+        for x in range(len(img_pixel_brightness[0])): # represents the x point iterator
+            img_pixel_brightness[y][x] = (sum(img.getpixel((x,y)))) // 3
+
+    return img_pixel_brightness
 
 def encoding_int_to_char(ascii_chars):
     comparator = np.linspace(0, 255, len(ascii_chars), dtype="int32")
@@ -44,65 +61,42 @@ def brightness_to_ascii(brightness, encoder):
 
     return ascii_image
 
-# Read the image 
-img = Image.open("ww.jpeg") # Initialize variable named "img" with Image class from PIL
-
-# Display the image
-# image.show()
-
-# 2. Load your image’s pixel data into a 2-dimensional array
-# Initialize values for image's width and height
-img_width = img.width
-img_height = img.height
-img_pixel = img_width * img_height
-
-# To get a pixel data, iterate through image's width and height
-
-def get_pixel(img):
-    img_pixel_rgb = np.zeros((3, img_height, img_width))
-
-    for channel in range(len(img_pixel_rgb)): # represents the channel iterator
-        for y in range(len(img_pixel_rgb[0])): # represents the y point iterator
-            for x in range(len(img_pixel_rgb[0][0])): # represents the x point iterator
-                img_pixel_rgb[channel][y][x] = img.getpixel((x,y))[channel]
-
-    return img_pixel_rgb
-
-img_pixel_rgb = get_pixel(img)
-
-# 3. Convert the RGB pixels into single brightness pixels
-# by using the average method: (R + G + B) / 3
-def img_to_brightness_pixel(img):
-    img_height = img.height
-    img_width = img.width
-    img_pixel_brightness = np.zeros((img_height, img_width), dtype="int32") # initialize a variable that stores brightness pixels
-
-    for y in range(len(img_pixel_brightness)): # represents the y point iterator
-        for x in range(len(img_pixel_brightness[0])): # represents the x point iterator
-            img_pixel_brightness[y][x] = (sum(img.getpixel((x,y)))) // 3
-
-    return img_pixel_brightness
-
-img_pixel_brightness = img_to_brightness_pixel(img)
-
-# 4. Convert brightness numbers to ASCII characters
-# BEGIN
-
-ascii_chars = "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"
-encoder = encoding_int_to_char(ascii_chars)
-
-ascii_image = brightness_to_ascii(img_pixel_brightness, encoder)
-# ascii_image = np.array(ascii_image)
-# ascii_image = np.reshape(ascii_image, (img_height, img_width))
-# print(ascii_image.shape)
-
-# 5. Print the ASCII art
 def display_ascii_image(ascii_image):
     for y in range(len(ascii_image)):
         for x in range(len(ascii_image[0])):
             print(ascii_image[y][x], end="")
         print("\n")
 
+# 1. Read the image 
+# BEGIN
+img = Image.open("ww.jpeg") # Initialize variable named "img" with Image class from PIL
+# END
+
+# 2. Load your image’s pixel data into a 2-dimensional array
+# Initialize values for image's width and height
+# BEGIN
+
+# To get a pixel data, iterate through image's width and height
+img_pixel_rgb = get_pixel(img)
+# END
+
+# 3. Convert the RGB pixels into single brightness pixels
+# by using the average method: (R + G + B) / 3
+img_pixel_brightness = img_to_brightness_pixel(img)
+
+# 4. Convert brightness numbers to ASCII characters
+# BEGIN
+ascii_chars = "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"
+encoder = encoding_int_to_char(ascii_chars)
+
+ascii_image = brightness_to_ascii(img_pixel_brightness, encoder)
+# END
+
+# 5. Print the ASCII art
+# BEGIN
 display_ascii_image(ascii_image)
+# END
+
+
 
 img.show()
